a16406e1c5456a43818a5a6f35aa4659b73f4dad
Julio <Julio@192.168.1.133>
2019-07-30 00:38:53 +0200

    Mejoras: *Diferentes muertes y lugares *El bot avisa 5 mins antes de la muerte de quien se va a enfrentar

--- Onda/src/main/java/KillerBot/Onda/Tarea.java
+++ Onda/src/main/java/KillerBot/Onda/Tarea.java
@@ -13,6 +13,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 import javax.imageio.ImageIO;
 
@@ -24,13 +25,17 @@ import twitter4j.conf.ConfigurationBuilder;
 
 public class Tarea extends TimerTask{
 	public BufferedImage image;
- //	static String[] nombres = {"Julio","Nacho","Fernando","Roni","Mark","Darlyn",
-//		"Carlos","Oscar","Naxo","Cervello","Pedro","Ivan","Juanjo","Perolo","Sarasa",
-//		"David","Lluis","Contreras","Rober","Ismael"};
 	
-	static Map<String, String> mape = new HashMap<String, String>(100);
-	String[] nombres = new String[105];
-	
+	static Map<String, String> mape = new HashMap<String, String>(100); //HashMap donde vinculo cada nombre con su cuenta
+	String[] nombres = new String[200]; //lista de los que estan vivos (nombres), muertos y formas de morir y lugares
+	String[] muertos = new String[200];
+	static String[] muertes = {"  ha matado de una apuñalada por la espalda", " ha matado con un cubata envenenado", " ha atropellado con el coche", 
+						" ha matado con un palo", " le ha metido 20 navajazos", " le ha dado una paliza", " le ha dado un botellazo en la cabeza", " mientras dormía",
+						" ha matado por exceso de sexo", " ha matado de un susto", " le ha desmembrado las extremidades",
+						" le ha reventado la cabeza con un bate", " le ha comido el cuello", " le ha matado de asco", " se ha comido",
+						" le ha hecho cosquillas hasta morir", " ha prendido fuego", " ha enterrado vivo"};
+	static String[] lugares = {" en el Mercadona"," en el Castillo", " debajo un punte", " en el museo del Taulell", " en el instituto",
+						" en el Salvador", " en el calvario", " en los Carmelitas", " en la piazzeta", " en las 3 Culturas", " en su casa"};
     
 	
 	public Tarea(){
@@ -142,44 +147,86 @@ public class Tarea extends TimerTask{
 		mape.put("Julio Perez", "julio_vcf");
 		mape.put("Nekane Iserte", null);
 		mape.put("Pedro Berrocal", null);
-//		mape.put("Rafa Perez", null);
-//		mape.put("Fernando Villegas", "fernando_RCK");
-//		mape.put("Anna Romero", "anna_romero9");
-//		mape.put("Claudia Catalan", "catalanclaudia9");
+		mape.put("Rafa Perez", null);
+		mape.put("Fernando Villegas", "fernando_RCK");
+		mape.put("Anna Romero", "anna_romero9");
+		mape.put("Claudia Catalan", "catalanclaudia9");
 		
-		nombres = listaDeNombres(mape);
-			
+		nombres = listaDeNombres(mape); //La lista nombres coge los nombres del Map
+
 	}
 	@Override
 	public void run() {
 //		try {
 //			crearImagen(nombres);
 //		} catch (IOException e) {
-//			// TODO Auto-generated catch block
+//			// TODO Auto-generated catch block					/*Con esto creo la primera imagen cuando voy a empezar*/
 //			e.printStackTrace();
 //		} 
 		nombres = duelo(nombres);
-		if(nombres.length == 1) {
-//			twitterServicio(nombres[0],null,1);
-//			System.out.println(nombres[0]+" se los ha cargado a todos");
-			
+		if(nombres.length == 1) {		/*el metodo run ejecuta el metodo duelo hasta que solo queda un superviviente*/
 			cancel();
 		}
+//		int contV =0; 
+//		int contF = 0;
+//		for(int i=0;i<200;i++){
+//			
+//			if(resucitar() == true)contV++;
+//			else contF++;
+//			
+//		}
+//		System.out.println(contV);
+//		System.out.println(contF);
+//		cancel();
 		
 	} 
 	
-	public String[] duelo(String[] lista) {
+	public String[] duelo(String[] lista) { //Metodo principal que enfrente a la gente y llama al resto de metodos
 		int n = (int) (Math.random() * lista.length) + 1;
 		int n1 = (int) (Math.random() * lista.length) + 1;
+		int m = (int) (Math.random() * muertes.length) + 1;			/*se elige a los dos oponentes y la forma de morir y lugar*/
+		int l = (int) (Math.random() * lugares.length) + 1;
 		int cont = lista.length;
-		String x = lista[n - 1];
-		String xt= mape.get(x);
+		String x = lista[n-1];
+		String asesino, xt;			/*Con la mejora de que avise 5 mins antes ahora x no siempre es el asesino*/
+		String asesinado, yt;
+		
 		String y = lista[n1 - 1];
 		while (n == n1) {
 			n1 = (int) (Math.random() * lista.length) + 1;
 			y = lista[n1 - 1];
 		}
-		String yt = mape.get(y);
+		twitterAviso(x,y);
+		xt = mape.get(x);
+		yt = mape.get(y);
+		System.out.println("En 5 mins se van a enfrentar " + x + " " + xt + " vs " + y + " " + yt + " solo uno de los dos seguirá con vida. Suerte!");
+		if(headsOrTails()== true){
+			asesino = x;
+			asesinado = y;
+			xt= mape.get(asesino);
+			yt= mape.get(asesinado);
+		}
+		else{
+			asesino = y;
+			asesinado = x;
+			n1 = n;
+			yt= mape.get(asesino);
+			xt= mape.get(asesinado);
+		}
+		if(xt == null) xt="";
+		if(yt == null) yt="";
+		
+		
+		try {
+			TimeUnit.MINUTES.sleep(1);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		String muerte = muertes[m-1];
+		String lugar = lugares[l-1];
 		String [] aux = asesinado(lista,n1-1);
 		try {
 			crearImagen(aux);
@@ -187,36 +234,25 @@ public class Tarea extends TimerTask{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		String[]
-		if (cont>2){	
+		
+		if(cont>2){	
 			cont--;
-			if(xt == null && yt == null){
-				twitterServicio(x,y,cont);
-				System.out.println(x + " ha matado a " + y + " quedan " + cont + " ondenses. #OndaWarBot");
-			}
-			else if(xt == null){
-				twitterServicio(x,y,cont);
-				System.out.println(x + " ha matado a " + y + " " + yt + " quedan " + cont + " ondenses. #OndaWarBot");
-			}
-			else if(yt == null){
-				twitterServicio(x,y,cont);
-				System.out.println(x + " " + xt + " ha matado a " + y + " quedan " + cont + " ondenses. #OndaWarBot");
-			}
-			else{ System.out.println(x + " " + xt + " ha matado a " + y + " " + yt + " quedan " + cont + " ondenses. #OndaWarBot");
-			twitterServicio(x,y,cont);
-				}
-			}
+			
+			
+			System.out.println(asesino + " " + xt + " ha matado a " + asesinado + " " + yt + muerte + lugar + " quedan " + cont + " ondenses. #OndaWarBot");
+			twitterServicio(asesino,asesinado,cont);
+		}
 		else{ 
 			cont--;
-			System.out.println(x + " " + xt +" ha matado a " + y + " " + yt +", " + x + " se los ha cargado a todos. #OndaWarBot");
-		twitterServicio(x,y,cont);
+			System.out.println(asesino + " " + xt +" ha matado a " + asesinado + " " + yt + muerte + lugar + ", " + x + " se los ha cargado a todos. #OndaWarBot");
+		twitterServicio(asesino,asesinado,cont);
 		}
 		
 		return asesinado(lista, n1-1);
-
+		
 	}
 
-	public static String[] asesinado(String[] arr, int index) {
+	public static String[] asesinado(String[] arr, int index) { //Metodo que elimina al asesinado de la lista 
 
 		if (arr == null || index < 0 || index >= arr.length) {
 			return arr;
@@ -243,17 +279,26 @@ public class Tarea extends TimerTask{
 		// return the resultant array
 		return anotherArray;
 	}
+//	public static String[] resucitado(String[] lista, String[] muertos){
+//		String[] aux = new String[lista.length-1];							/*el metodo de resucitar esta por desarrollar*/
+//		return aux;
+//	}
   
-	public static void twitterServicio(String asesino, String asesinado,int conta){
+	public static void twitterServicio(String asesino, String asesinado,int conta){ //metodo que usa twitter para pubicar quien muere
 //		String consumerKey = "OXd5oPvVPhXqE1i856SZSeZ9R";
 //		String consumerSecret = "XRT9sdX4Q35wNGDIwRSX5QrMcjPqrm5lUIS6jJvNeDKyAOAQ0g";
-//		String accessToken = "1149316675877167104-ZIOKZAH7SZ361PVsGTMucFDh11xk9v";
+//		String accessToken = "1149316675877167104-ZIOKZAH7SZ361PVsGTMucFDh11xk9v";		/*Acceso a la cuenta principal*/
 //		String accessTokenSecret = "2kvJHQ2uwPFro5ZBlw1bu2J7UGJnpWSMkhogmbrYu2u6J";
 		
 		String consumerKey = "KdpN6MZxxQy1dKbQPOuruSfxT";
 		String consumerSecret = "Zw2FfKtbZqzjIcWebiAG0rDPkXkczplpg7ojHUHifI7J3dnI68";
-		String accessToken = "1151775525062479872-WrvedHQYmQIDlag9o9ZvjCqPTHtJG5";
+		String accessToken = "1151775525062479872-WrvedHQYmQIDlag9o9ZvjCqPTHtJG5";		/*Acceso a la cuenta de pruebas*/
 		String accessTokenSecret = "Ux4PobRTXL5j7meKdEhaFElAVlzJN8KxZI1qreBA2u4wp";
+		
+		int m = (int) (Math.random() * muertes.length) + 1;			/*se elige a los dos oponentes y la forma de morir y lugar*/
+		int l = (int) (Math.random() * lugares.length) + 1;
+		String muerte = muertes[m-1];
+		String lugar = lugares[l-1];
 		
 		ConfigurationBuilder cb = new ConfigurationBuilder();
 		cb.setDebugEnabled(true).setOAuthConsumerKey(consumerKey).setOAuthConsumerSecret(consumerSecret)
@@ -270,14 +315,14 @@ public class Tarea extends TimerTask{
 		try {
 			if(conta > 1){
 				File file = new File("C:/Users/Julio/workspace/github/Onda/Text.jpg");
-				statusMessage = (asesino+ " " + xt +" ha matado a "+ asesinado + " " + yt + " quedan "+ conta + " ondenses. #OndaWarBot");
+				statusMessage = (asesino + " " + xt + muerte + " a " + asesinado + " " + yt + lugar + ", quedan " + conta + " ondenses. #OndaWarBot");
 				StatusUpdate status = new StatusUpdate(statusMessage);
 				status.setMedia(file);
 				twitter.updateStatus(status);
 			}
 			else{
 				File file = new File("C:/Users/Julio/workspace/github/Onda/win.png");
-				statusMessage = (asesino+" "+ xt +" ha matado a "+asesinado + " "+ yt + ", se los ha cargado a todos. #OndaWarBot ");
+				statusMessage = (asesino + " " + xt + muerte + " a " + asesinado + " " + yt + lugar + ", se los ha cargado a todos. #OndaWarBot ");
 				StatusUpdate status = new StatusUpdate(statusMessage);
 				status.setMedia(file);
 				twitter.updateStatus(status);
@@ -288,31 +333,45 @@ public class Tarea extends TimerTask{
 		}
 	}
 	
-//	public void crearImagen(){
-//		if(image==null) return;
-//		try{
-//			Graphics g = image.getGraphics();
-//			g.setColor(Color.red);	
-//			g.drawString(nombres[0], 100, 100);
-//			g.drawString(nombres[1], 100, 100);
-////			g.drawImage(ImageIO.read(new File("C:/Users/Julio/Desktop/prueba.jpg")), 300, 300, null);
-//			ImageIO.write(image, "png", new File("prueba.png"));
-//		}catch (IOException e) {e.printStackTrace();}
-//	}
-	
-	public static String[] listaDeNombres(Map<String,String> lista){
-		String[] claves = lista.keySet().toArray(new String[0]);
-		 
-//		System.out.println(claves.length);
-		Arrays.sort(claves);
-//		for (String clave: claves)
-//			  System.out.println(clave);
+	public static void twitterAviso(String asesino, String asesinado){
+//		String consumerKey = "OXd5oPvVPhXqE1i856SZSeZ9R";
+//		String consumerSecret = "XRT9sdX4Q35wNGDIwRSX5QrMcjPqrm5lUIS6jJvNeDKyAOAQ0g";
+//		String accessToken = "1149316675877167104-ZIOKZAH7SZ361PVsGTMucFDh11xk9v";
+//		String accessTokenSecret = "2kvJHQ2uwPFro5ZBlw1bu2J7UGJnpWSMkhogmbrYu2u6J";
 		
-		return claves;
+		String consumerKey = "KdpN6MZxxQy1dKbQPOuruSfxT";
+		String consumerSecret = "Zw2FfKtbZqzjIcWebiAG0rDPkXkczplpg7ojHUHifI7J3dnI68";
+		String accessToken = "1151775525062479872-WrvedHQYmQIDlag9o9ZvjCqPTHtJG5";
+		String accessTokenSecret = "Ux4PobRTXL5j7meKdEhaFElAVlzJN8KxZI1qreBA2u4wp";
 		
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true).setOAuthConsumerKey(consumerKey).setOAuthConsumerSecret(consumerSecret)
+				.setOAuthAccessToken(accessToken).setOAuthAccessTokenSecret(accessTokenSecret);
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
+				
+		String xt = mape.get(asesino);
+		if (xt==null)xt="";
+		String yt = mape.get(asesinado);
+		if (yt==null)yt="";
+		
+		try {
+			twitter.updateStatus("En 5 mins se van a enfrentar " + asesino + " " + xt + " vs " + asesinado + " " + yt + " solo uno de los dos seguirá con vida. Suerte!");
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void crearImagen(String nombres[]) throws IOException{
+
+	
+	public static String[] listaDeNombres(Map<String,String> lista){ //metodo que genera los nombres a partir del Map
+		String[] claves = lista.keySet().toArray(new String[0]);
+		Arrays.sort(claves);
+	
+		return claves;
+	}
+	
+	public void crearImagen(String nombres[]) throws IOException{ //metodo que crea la imagen de la gente que queda viva
 		BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
 	    Graphics2D g2d = img.createGraphics();
 	    Font font = new Font("Arial", Font.BOLD, 12);
@@ -413,4 +472,28 @@ public class Tarea extends TimerTask{
 	    	
 	    }
 	}
+	
+	 public static boolean headsOrTails() //metodo cara o cruz que elige si muere x o y
+	 {
+	   boolean coinState;
+	   if (Math.random() <= 0.5) {//heads 50% of the time 
+	     coinState = true; //heads
+	   }
+	   else {    
+	     coinState = false; //tails
+	   }
+	   return coinState;
+	 }
+	 
+	 public static boolean resucitar() //metodo que dice si alguien va a resucitar en esta hora
+	 {
+	   boolean resucita;
+	   if (Math.random() <= 0.03) {//3% de probabilidad de que alguien resucite
+	     resucita = true; 
+	   }
+	   else {    
+	     resucita = false; 
+	   }
+	   return resucita;
+	 }
 }
